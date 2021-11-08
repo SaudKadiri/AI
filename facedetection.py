67d7bf6af45df698a36fee3e47bf2090203de7b0
SaudKadiri <34000860+SaudKadiri@users.noreply.github.com>
2021-11-08 23:26:19 +0530

    Get the file cascade

--- facedetection.py
+++ facedetection.py
@@ -7,6 +7,10 @@ import cv2
 # Get the image by using the imread() method
 img = cv2.imread("/path/to/image/file")
 
+# Get the face_cascade using the CascadeClassified the method asks for the file path which we can get using
+# cv2.data.haarcascades which gives out the path to various cascade files is concatenated with the required xml file name
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
 # Display the image using the imshow() method with two parameters: Window name, image_variable
 cv2.imshow('Image', img)
 
