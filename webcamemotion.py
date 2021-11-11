646b69c5974b8ee019aaf89788e1b98dec9c586f
SaudKadiri <34000860+SaudKadiri@users.noreply.github.com>
2021-11-11 19:27:11 +0530

    Write text on a frame. For now it's "emotion"

--- webcamemotion.py
+++ webcamemotion.py
@@ -19,6 +19,17 @@ while True:
     # Show the frame
     cv2.imshow('Live video cam', frame)
 
+    # Required parameters for put text
+    # image: The image on which the text is to be displayed. Remember each frame is an image!
+    # text: String(text) that is to be written on the image.
+    # org: Coordinates of the bottom-left corner of the text string in the image. The coordinates are represented as tuples of two values i.e. (X coordinate value, Y coordinate value).
+    # font: Font type of the text. Some of font types are FONT_HERSHEY_SIMPLEX, FONT_HERSHEY_PLAIN, , etc.
+    # fontScale: Font scale factor that is multiplied by the font-specific base size.
+    # color: It is the color of text string to be drawn. For BGR, we pass a tuple. eg: (255, 0, 0) for blue color.
+    # thickness: It is the thickness of the line in px.
+    #           Image,   Text,        Org,         Font type,      font scale,  color,    thickness
+    cv2.putText(frame, "Emotion", (450, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 5)
+    
     # The function waitKey waits for a key event infinitely (when \texttt{delay}\leq 0 ) or for delay milliseconds, when it is positive
     c = cv2.waitKey(1)
     if c == 27:
