33f45b84db8616de6d7991b774eebd41be90127d
Saud Kadiri <34000860+SaudKadiri@users.noreply.github.com>
2022-06-24 15:04:46 +0530

    Breadth first search implemented in python

---
+++ bfs.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def bfs(start, goal, graph):
+    '''
+    Finds the path using bfs
+    '''
+    path = []
+    frontier = deque()  # Queue
+    explored = set()
+    frontier.append(start)
+    while frontier:
+        current = frontier.popleft()
+        path.append(current)
+        if current == goal:
+            break
+        explored.add(current)
+        for next in graph.get(current, []):
+            if next not in explored:
+                frontier.append(next)
+    else:
+        raise ValueError(f'Failure: No path from {start} to {goal}')
+    return ' -> '.join(path)
+
+
+graph = {
+    "A": ["B", "C", "D"],
+    "B": ["E", "F"],
+    "C": ["G", "H"],
+    "D": ["I", "J"]
+}
+start, goal = 'A', 'H'
+print('BFS traversal: ', bfs(start, goal, graph))
